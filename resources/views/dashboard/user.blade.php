da4199709161bf5de9f5118a9bfb8ed78e90cb20
satriaNaufal18 <satria182002@gmail.com>
2023-12-17 00:46:48 +0700

    anu

--- resources/views/dashboard/user.blade.php
+++ resources/views/dashboard/user.blade.php
@@ -44,7 +44,6 @@
 
 
     <div class="content-backdrop fade"></div>
-    </div>
     <!-- Footer -->
     <footer class="content-footer footer bg-footer-theme">
         <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
@@ -61,6 +60,5 @@
 
     <!-- Overlay -->
     <div class="layout-overlay layout-menu-toggle"></div>
-    </div>
     <!-- / Layout wrapper -->
 @endsection
